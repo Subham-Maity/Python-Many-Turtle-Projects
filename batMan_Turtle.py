f612b463de927a58cff94c79a0822ee8a85ac4a7
Subham Maity <codexam.personal@gmail.com>
2022-02-23 07:34:36 +0530

    Update batMan_Turtle.py
    Changing code in batman.py

--- batMan_Turtle.py
+++ batMan_Turtle.py
@@ -1,75 +1,75 @@
 import turtle
 import math
 
-kalam = turtle.Turtle()
-kalam.speed(500)
+xam = turtle.Turtle()
+xam.speed(500)
 
 window = turtle.Screen()
 window.bgcolor("#000000")
-kalam.color("yellow")
+xam.color("yellow")
 
-ankur = 20
+Xam2 = 20
 
-kalam.left(90)
-kalam.penup()
-kalam.goto(-7 * ankur, 0)
-kalam.pendown()
+xam.left(90)
+xam.penup()
+xam.goto(-7 * Xam2, 0)
+xam.pendown()
 
-for a in range(-7 * ankur, -3 * ankur, 1):
-    x = a / ankur
+for a in range(-7 * Xam2, -3 * Xam2, 1):
+    x = a / Xam2
     rel = math.fabs(x)
     y = 1.5 * math.sqrt((-math.fabs(rel - 1)) * math.fabs(3 - rel) / ((rel - 1) * (3 - rel))) * (
                 1 + math.fabs(rel - 3) / (rel - 3)) * math.sqrt(1 - (x / 7) ** 2) + (
                     4.5 + 0.75 * (math.fabs(x - 0.5) + math.fabs(x + 0.5)) - 2.75 * (
                         math.fabs(x - 0.75) + math.fabs(x + 0.75))) * (1 + math.fabs(1 - rel) / (1 - rel))
-    kalam.goto(a, y * ankur)
+    xam.goto(a, y * Xam2)
 
-for a in range(-3 * ankur, -1 * ankur - 1, 1):
-    x = a / ankur
+for a in range(-3 * Xam2, -1 * Xam2 - 1, 1):
+    x = a / Xam2
     rel = math.fabs(x)
     y = (2.71052 + 1.5 - 0.5 * rel - 1.35526 * math.sqrt(4 - (rel - 1) ** 2)) * math.sqrt(
         math.fabs(rel - 1) / (rel - 1))
-    kalam.goto(a, y * ankur)
+    xam.goto(a, y * Xam2)
 
-kalam.goto(-1 * ankur, 3 * ankur)
-kalam.goto(int(-0.5 * ankur), int(2.2 * ankur))
-kalam.goto(int(0.5 * ankur), int(2.2 * ankur))
-kalam.goto(1 * ankur, 3 * ankur)
+xam.goto(-1 * Xam2, 3 * Xam2)
+xam.goto(int(-0.5 * Xam2), int(2.2 * Xam2))
+xam.goto(int(0.5 * Xam2), int(2.2 * Xam2))
+xam.goto(1 * Xam2, 3 * Xam2)
 print("Batman Logo with Python Turtle")
-for a in range(1 * ankur + 1, 3 * ankur + 1, 1):
-    x = a / ankur
+for a in range(1 * Xam2 + 1, 3 * Xam2 + 1, 1):
+    x = a / Xam2
     rel = math.fabs(x)
     y = (2.71052 + 1.5 - 0.5 * rel - 1.35526 * math.sqrt(4 - (rel - 1) ** 2)) * math.sqrt(
         math.fabs(rel - 1) / (rel - 1))
-    kalam.goto(a, y * ankur)
+    xam.goto(a, y * Xam2)
 
-for a in range(3 * ankur + 1, 7 * ankur + 1, 1):
-    x = a / ankur
+for a in range(3 * Xam2 + 1, 7 * Xam2 + 1, 1):
+    x = a / Xam2
     rel = math.fabs(x)
     y = 1.5 * math.sqrt((-math.fabs(rel - 1)) * math.fabs(3 - rel) / ((rel - 1) * (3 - rel))) * (
                 1 + math.fabs(rel - 3) / (rel - 3)) * math.sqrt(1 - (x / 7) ** 2) + (
                     4.5 + 0.75 * (math.fabs(x - 0.5) + math.fabs(x + 0.5)) - 2.75 * (
                         math.fabs(x - 0.75) + math.fabs(x + 0.75))) * (1 + math.fabs(1 - rel) / (1 - rel))
-    kalam.goto(a, y * ankur)
+    xam.goto(a, y * Xam2)
 
-for a in range(7 * ankur, 4 * ankur, -1):
-    x = a / ankur
+for a in range(7 * Xam2, 4 * Xam2, -1):
+    x = a / Xam2
     rel = math.fabs(x)
     y = (-3) * math.sqrt(1 - (x / 7) ** 2) * math.sqrt(math.fabs(rel - 4) / (rel - 4))
-    kalam.goto(a, y * ankur)
+    xam.goto(a, y * Xam2)
 
-for a in range(4 * ankur, -4 * ankur, -1):
-    x = a / ankur
+for a in range(4 * Xam2, -4 * Xam2, -1):
+    x = a / Xam2
     rel = math.fabs(x)
     y = math.fabs(x / 2) - 0.0913722 * x ** 2 - 3 + math.sqrt(1 - (math.fabs(rel - 2) - 1) ** 2)
-    kalam.goto(a, y * ankur)
+    xam.goto(a, y * Xam2)
 
-for a in range(-4 * ankur - 1, -7 * ankur - 1, -1):
-    x = a / ankur
+for a in range(-4 * Xam2 - 1, -7 * Xam2 - 1, -1):
+    x = a / Xam2
     rel = math.fabs(x)
     y = (-3) * math.sqrt(1 - (x / 7) ** 2) * math.sqrt(math.fabs(rel - 4) / (rel - 4))
-    kalam.goto(a, y * ankur)
+    xam.goto(a, y * Xam2)
 
-kalam.penup()
-kalam.goto(300, 300)
-turtle.done() 
+xam.penup()
+xam.goto(300, 300)
+turtle.done()
